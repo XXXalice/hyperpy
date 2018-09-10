fe069f78cb23b0018538fcbd01820b3c36b49e1d
梅宮 <euthanasia045@gmail.com>
2018-09-10 22:37:18 +0900

    pipenv test add

---
+++ pipenv_test/testtt.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print("hello coolguys!! today, i i will teach you Pi.")
+print("Pi is {} its awesome!!!! thank you for running this script!!! see you coolguys..".format(str(np.pi)))
